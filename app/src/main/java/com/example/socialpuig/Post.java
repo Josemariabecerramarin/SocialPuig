ff5949373d6e04f292a71bfc9b4a539dd51c71d5
jbecerra <josemariabecerra30@gmail.com>
2022-03-02 17:15:41 +0100

    Initial commit

---
+++ app/src/main/java/com/example/socialpuig/Post.java
@@ -0,0 +1,29 @@
+package com.example.socialpuig;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Post {
+    public String uid;
+    public String author;
+    public String authorPhotoUrl;
+    public String content;
+    public Map<String, Boolean> likes = new HashMap<>();
+    public String mediaUrl;
+    public String mediaType;
+    public long timestamp;
+    public String id;
+    // Constructor vacio requerido por Firestore
+    public Post(){}
+
+    public Post(String uid, String author, String authorPhotoUrl, String content, String mediaUrl, String mediaType, long timestamp) {
+        this.uid = uid;
+        this.author = author;
+        this.authorPhotoUrl = authorPhotoUrl;
+        this.content = content;
+        this.mediaUrl = mediaUrl;
+        this.mediaType = mediaType;
+        this.timestamp = timestamp;
+    }
+}
